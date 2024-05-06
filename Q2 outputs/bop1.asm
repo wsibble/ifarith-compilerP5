bece6f388109c9d3d8548de39b3918cad2318fa6
PaulCIV <plclark@syr.edu>
2024-05-06 00:06:53 -0400

    LAST ONE

--- Q2 outputs/bop1.asm
+++ Q2 outputs/bop1.asm
@@ -19,52 +19,52 @@ _main:	push rbp
 	mov esi, 89
 	mov [rbp-8], esi
 	mov esi, 3
-	mov [rbp-96], esi
+	mov [rbp-48], esi
 	mov esi, [rbp-8]
+	mov [rbp-40], esi
+	mov edi, [rbp-48]
+	mov eax, [rbp-40]
+	imul eax, edi
+	mov [rbp-40], eax
+	mov esi, [rbp-40]
+	mov [rbp-16], esi
+	mov esi, 4
+	mov [rbp-32], esi
+	mov esi, [rbp-32]
+	mov [rbp-24], esi
+	mov edi, [rbp-16]
+	mov eax, [rbp-24]
+	imul eax, edi
+	mov [rbp-24], eax
+	mov esi, 9
+	mov [rbp-96], esi
+	mov esi, [rbp-16]
 	mov [rbp-88], esi
 	mov edi, [rbp-96]
 	mov eax, [rbp-88]
-	imul eax, edi
+	add eax, edi
 	mov [rbp-88], eax
-	mov esi, [rbp-88]
-	mov [rbp-16], esi
-	mov esi, 4
+	mov esi, [rbp-24]
 	mov [rbp-80], esi
+	mov edi, [rbp-88]
+	mov eax, [rbp-80]
+	add eax, edi
+	mov [rbp-80], eax
 	mov esi, [rbp-80]
-	mov [rbp-72], esi
-	mov edi, [rbp-16]
-	mov eax, [rbp-72]
-	imul eax, edi
-	mov [rbp-72], eax
-	mov esi, 9
-	mov [rbp-64], esi
-	mov esi, [rbp-16]
 	mov [rbp-56], esi
-	mov edi, [rbp-64]
-	mov eax, [rbp-56]
-	add eax, edi
-	mov [rbp-56], eax
-	mov esi, [rbp-72]
-	mov [rbp-48], esi
+	mov esi, [rbp-16]
+	mov [rbp-72], esi
 	mov edi, [rbp-56]
-	mov eax, [rbp-48]
-	add eax, edi
-	mov [rbp-48], eax
-	mov esi, [rbp-48]
-	mov [rbp-24], esi
-	mov esi, [rbp-16]
-	mov [rbp-40], esi
-	mov edi, [rbp-24]
-	mov eax, [rbp-40]
+	mov eax, [rbp-72]
 	sub eax, edi
-	mov [rbp-40], eax
+	mov [rbp-72], eax
 	mov esi, [rbp-16]
-	mov [rbp-32], esi
-	mov edi, [rbp-40]
-	mov eax, [rbp-32]
+	mov [rbp-64], esi
+	mov edi, [rbp-72]
+	mov eax, [rbp-64]
 	imul eax, edi
-	mov [rbp-32], eax
-	mov esi, [rbp-32]
+	mov [rbp-64], eax
+	mov esi, [rbp-64]
 	lea rdi, [rel int_format]
 	mov eax, 0
 	call _printf
